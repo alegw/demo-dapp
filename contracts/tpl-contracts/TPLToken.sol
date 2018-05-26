dd1ec5f76396650ad95cc2b20e0ab5c4138a1bcd
Alejandro Machado <alejandro@macha.do>
2018-05-26 18:30:53 -0300

    Adapt to local environment

---
+++ contracts/tpl-contracts/TPLToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.19;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "./checks/TransactionChecker.sol";
+
+contract TPLToken is StandardToken {
+
+  TransactionChecker validator;
+
+  function TPLToken(TransactionChecker _validator) public {
+    validator = _validator;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(validator.transferAllowed(msg.sender, _to, _value));
+    super.transfer(_to, _value);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(validator.transferAllowed(msg.sender, _to, _value));
+    super.transferFrom(_from, _to, _value);
+  }
+
+}
