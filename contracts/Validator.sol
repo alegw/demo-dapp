b787fda3a3e0714f25e42fd36bd0875ac385253c
Alejandro Machado <alejandro@macha.do>
2018-05-26 18:56:38 -0300

    Downsize app

--- contracts/Validator.sol
+++ contracts/Validator.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.4.21;
 
 import { Jurisdiction } from "./tpl-contracts/Jurisdiction.sol";
 
-
-contract DummyValidator {
+contract Validator {
 
   Jurisdiction jurisdiction;
 
-  function DummyValidator(Jurisdiction _jurisdiction) public {
+  function Validator(Jurisdiction _jurisdiction) public {
     jurisdiction = _jurisdiction;
   }
 
+  // There should be a function that reads or gets a message from
+  // our backend and adds
   function validate() public {
     jurisdiction.addAttribute(msg.sender, "VALID", 1);
   }
